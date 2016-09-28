51a9f686f262a915e53310cb6fb9656bdc1ba198
SprintZ <zrcoding@gmail.com>
2016-09-28 00:07:34 -0700

    Basic Version of MyCrawler
    1.00 version basic function

---
+++ MyCrawler/src/com/example/crawler/CrawlStat.java
@@ -0,0 +1,46 @@
+package com.example.crawler;
+
+import java.util.ArrayList;
+
+public class CrawlStat {
+	private int totalProcessedPages;
+	private long totalLinks;
+	private long totalTextSize;
+	public ArrayList<MyPage> pageLists = new ArrayList<MyPage>();
+
+	public int getTotalProcessedPages() {
+		return totalProcessedPages;
+	}
+
+	public void setTotalProcessedPages(int totalProcessedPages) {
+		this.totalProcessedPages = totalProcessedPages;
+	}
+
+	public void incProcessedPages() {
+		this.totalProcessedPages++;
+	}
+
+	public long getTotalLinks() {
+		return totalLinks;
+	}
+
+	public void setTotalLinks(long totalLinks) {
+		this.totalLinks = totalLinks;
+	}
+
+	public long getTotalTextSize() {
+		return totalTextSize;
+	}
+
+	public void setTotalTextSize(long totalTextSize) {
+		this.totalTextSize = totalTextSize;
+	}
+
+	public void incTotalLinks(int count) {
+		this.totalLinks += count;
+	}
+
+	public void incTotalTextSize(int count) {
+		this.totalTextSize += count;
+	}
+}
---
+++ MyCrawler/src/com/example/crawler/MyPage.java
@@ -0,0 +1,27 @@
+package com.example.crawler;
+
+public class MyPage {
+	private int statusCode;
+	private String url;
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public MyPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
